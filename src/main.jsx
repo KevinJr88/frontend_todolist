daa6625a051dbc1c0c27f7bb803d521e0a3779b6
kevin.rahadinata <kevin.rahadinata@bcadigital.co.id>
2024-11-18 12:12:07 +0700

    coba todolist

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 // import AppRouter from './routes/index';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Todolist from './pages/to-do-list';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    <App />
+    <Todolist />
   </React.StrictMode>,
 )
