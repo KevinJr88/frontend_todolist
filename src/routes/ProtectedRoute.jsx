3edf0c7fa6f29c629fe6e14a278b7293402fdb18
kevin.rahadinata <kevin.rahadinata@bcadigital.co.id>
2024-11-18 11:34:36 +0700

    update

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
+// import React, { useEffect } from 'react';
+// import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ children }) => {
-    if (!localStorage.getItem("isAuthenticated")) {
-        return <Navigate to="/login" />;
-    }
+// const ProtectedRoute = ({ children }) => {
+//     if (!localStorage.getItem("isAuthenticated")) {
+//         return <Navigate to="/login" />;
+//     }
 
-    return children;
-};
+//     return children;
+// };
 
-export default ProtectedRoute;
+// export default ProtectedRoute;
